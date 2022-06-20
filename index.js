18fd2d0c11032eb3cc13695b1e451b2fe9ccab1a
Sergey Kozlov <dartess@mail.ru>
2022-06-20 17:52:32 +0300

    update demo images

---
+++ index.js
@@ -0,0 +1,45 @@
+import got from 'got';
+import * as PImage from 'pureimage';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+await PImage.registerFont('Roboto.ttf', 'Roboto').load();
+
+const data = await got.get('https://raw.githubusercontent.com/onderceylan/pwa-asset-generator/master/src/config/apple-fallback-data.json').json();
+
+const res = new Set();
+
+data.forEach(({ portrait, landscape, scaleFactor }) => {
+    Object.entries({ portrait, landscape }).forEach(([orientation, { width, height }]) => {
+        const deciceWidth = (orientation === 'portrait' ? width : height) / scaleFactor;
+        const deciceHeight = (orientation === 'portrait' ? height : width) / scaleFactor;
+
+        generateImage(width, height);
+        generateImage(width, height, true);
+        res.add(`<link rel="apple-touch-startup-image" href="./splash-screens/apple-splash-${width}-${height}.png" media="(device-width: ${deciceWidth}px) and (device-height: ${deciceHeight}px) and (-webkit-device-pixel-ratio: ${scaleFactor}) and (orientation: ${orientation})">`)
+        res.add(`<link rel="apple-touch-startup-image" href="./splash-screens/apple-splash-dark-${width}-${height}.png" media="(prefers-color-scheme: dark) and (device-width: ${deciceWidth}px) and (device-height: ${deciceHeight}px) and (-webkit-device-pixel-ratio: ${scaleFactor}) and (orientation: ${orientation})">`)
+    })
+});
+
+console.log([...res].join('\n'));
+
+async function generateImage(width, height, isDark) {
+    const img = PImage.make(width, height);
+    const ctx = img.getContext('2d');
+    
+    ctx.fillStyle = isDark ? '#0A043C' : '#FFE3D8';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = isDark ? '#FFFFFF' : '#000000';
+    ctx.fillRect(0, 0, 100, 100);
+    ctx.fillRect(width - 100, height - 100, width, height);
+    ctx.font = "48pt Roboto";
+    ctx.fillText(`${width} x ${height}${isDark ? ' (dark)' : ''}`, 150, 150);
+
+    const file = path.join(__dirname, `docs/splash-screens/apple-splash-${isDark ? 'dark-' : ''}${width}-${height}.png`)
+    console.log(file);
+    PImage.encodePNGToStream(img, fs.createWriteStream(file));
+}
